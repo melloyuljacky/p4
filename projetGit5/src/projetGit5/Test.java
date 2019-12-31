4eb26b96eb3e09db75c886eadf4c3ef4d222f19b
jacky <mellouljacky@hotmail.com>
2019-12-31 13:25:08 +0100

    p6 t1

---
+++ projetGit5/src/projetGit5/Test.java
@@ -0,0 +1,10 @@
+package projetGit5;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("p5  test1");
+
+	}
+
+}
---
+++ projetGit6/src/projetGit6/Test.java
@@ -0,0 +1,10 @@
+package projetGit6;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("p6 t1");
+
+	}
+
+}
